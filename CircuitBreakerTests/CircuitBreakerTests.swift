034b301358af93e99293c26c56ec22d8eaa9b6cc
fe9lix <fe9lix@googlemail.com>
2016-02-07 15:16:16 +0100

    Initial Commit

---
+++ CircuitBreakerTests/CircuitBreakerTests.swift
@@ -0,0 +1,36 @@
+//
+//  CircuitBreakerTests.swift
+//  CircuitBreakerTests
+//
+//  Created by Felix Raab on 07/02/2016.
+//  Copyright © 2016 betriebsraum. All rights reserved.
+//
+
+import XCTest
+@testable import CircuitBreaker
+
+class CircuitBreakerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
